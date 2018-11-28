80500996a0a50cc15a493cec053cbe9b7d6dcb95
ifirsandi <45285295+ifirsandi@users.noreply.github.com>
2018-11-28 20:35:40 +0700

    Create Practice 1(Beatiful Triplets).c
    Given a sequence of integers a, a triplet (a[i],a[j],a[k]) is beautiful if:
    - i<j<k
    - a[j]-a[i] = a[k]-a[j] = d 
    Given an increasing sequenc of integers and the value of "d", count the number of beautiful triplets in the sequence.
    For example, the sequence arr=[1, 2, 4, 5, 6, 7,10] and d=6. There are 1 beautiful triplets, by index: [i,j,k] = [1,2,4]  that, (ar[i], ar[j], ar[k])=(2,4,6)

---
+++ Practice 1(Beatiful Triplets).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// Complete the beautifulTriplets function below.
+
+int main()
+{
+    int n, d;
+    scanf("%d %d", &n, &d);
+    int ar[n];
+    for (int i=0; i<n; i++){
+        scanf("%d", &ar[i]);
+    }
+    int count=0;
+    for(int i=0; i<n; i++){
+        for (int j=i+1; j<n; j++){
+            if (ar[j]-ar[i] == d){
+                for (int k=j+1;k<n; k++){
+                    if (ar[k]-ar[j]== d){
+                        count++;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    printf("%d", count);
+    return 0;
+}
